cdc9b97cd1cc4be0ea3c051f6e10f48e7b308f75
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:44:02 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan14-Bit/src/if1/pkg10118005/latihan14/bit/IF110118005Latihan14Bit.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan14.bit;
+
+/**
+ *
+ * @author USER
+ * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program yang menampilkan bit
+ */
+public class IF110118005Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 60;/* 60 = 0011 1100 */
+        int b = 13;/* 13 = 0000 1100 */
+        int c = 0;
+        c = a & b;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /* -61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c);
+         c = a >> 2;
+         /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+        
+        
+             
+    }
+    
+}
